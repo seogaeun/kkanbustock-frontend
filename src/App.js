435e3accba642b0ab2fa8e08c7da6f3db61bca37
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-11 01:27:21 +0900

    [Edit]컴포넌트를 페이지로 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
-import Main from './components/Main/Main.js';
-import Login from './components/Login/Login.js';
+import Main from './pages/Main';
+import Login from './pages/Login';
 import Quiz from  './components/Quiz/Quiz.js';
 import Nav from './components/Nav/Nav.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
