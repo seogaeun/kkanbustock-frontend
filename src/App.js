ef87f4fbe112d7e650d6773d38a9abc1a27c8bba
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-11 01:05:30 +0900

    [Add]퀴즈 화면 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import Main from './components/Main/Main.js';
 import Login from './components/Login/Login.js';
+import Quiz from  './components/Quiz/Quiz.js';
 import Nav from './components/Nav/Nav.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main/>}/>
         <Route path="/My" element={<Login />} />
+        <Route path="/Quiz" element={<Quiz />} />
       </Routes>
     </div>
     </BrowserRouter>
