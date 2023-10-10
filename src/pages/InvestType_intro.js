5f5a87e180af37b4e591812af48383a2fb61671c
seogaeun <se0323333@gmail.com>
2023-10-10 20:16:50 +0900

    투자성향테스트 화면 수정

---
+++ src/pages/InvestType_intro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./../styles/InvestType_intro.css";
+import SOL_hello from "./../assets/images/sol-hello.png";
+
+let nickname = "게스트";
+
+function InvestType_intro() {
+    return (
+        <div className='center-container'>
+            <div className='InvestType_intro'>
+                <div className='intro-container'>
+                    <div className='intro-title'>
+                        <h2><span>{nickname}</span>님의</h2>
+                        <h1>투자성향테스트</h1>
+                    </div>
+                    <button>시작하기</button>
+                </div>
+                <img alt="안녕하세요 쏠" src={SOL_hello} />
+            </div>
+        </div>
+    );
+}
+
+export default InvestType_intro;
