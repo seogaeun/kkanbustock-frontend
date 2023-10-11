d308346af818d7d99b50731ea32a6539c9a52c8a
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-11 18:37:24 +0900

    [Add]필터링 함수 만들었다!

--- src/components/Quiz/QuizHistory/QuizHistory.js
+++ src/components/Quiz/QuizHistory/QuizHistory.js
@@ -1,41 +1,47 @@
-import React, { useState } from 'react';
-import './QuizHistory.css'
-import SolvedQuiz from '../SolvedQuiz/SolvedQuiz';
+import React, { useState } from "react";
+import "./QuizHistory.css";
+import SolvedQuiz from "../SolvedQuiz/SolvedQuiz";
 
 const QuizHistory = ({ quizData }) => {
-
-  const [selectedOption, setSelectedOption] = useState('');
-
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleSelectChange = (event) => {
-    console.log('Selected Option:', event.target.value);
+    console.log("Selected Option:", event.target.value);
     setSelectedOption(event.target.value);
   };
 
   const filteredQuizData = selectedOption
-    ? quizData.filter((quiz) => (quiz.isCorrect && selectedOption === '정답') || (!quiz.isCorrect && selectedOption === '오답'))
+    ? quizData.filter((quiz) => {
+        if (selectedOption === "정답" && quiz.isCorrect) {
+          return true;
+        } else if (selectedOption === "오답" && !quiz.isCorrect) {
+          return true;
+        }
+        return false;
+      })
     : quizData;
 
-    return (
-      <div className='quizHistory'>
+  return (
+    <div className="quizHistory">
+      <div className="quizHistoryHeader">
         <h2>문제 풀이 내역</h2>
-        <div>
         <select value={selectedOption} onChange={handleSelectChange}>
           <option value="">전체</option>
           <option value="정답">정답</option>
           <option value="오답">오답</option>
         </select>
       </div>
-        <div className='listHeader'>
-            <div className='no'>NO</div>
-            <div className='date'>날짜</div>
-            <div className='q'>문제</div>
-            <div className='a'>정답</div>
-            <div className='result'>결과</div>
-        </div>
+      <div className="listHeader">
+        <div className="no">NO</div>
+        <div className="date">날짜</div>
+        <div className="q">문제</div>
+        <div className="a">정답</div>
+        <div className="result">결과</div>
+      </div>
 
-        <ul>
-          {quizData && quizData.map((quiz, index) => (
+      <ul>
+        {filteredQuizData &&
+          filteredQuizData.map((quiz, index) => (
             <SolvedQuiz
               key={index}
               questionNumber={quiz.questionNumber}
@@ -46,9 +52,8 @@ const QuizHistory = ({ quizData }) => {
               isCorrect={quiz.isCorrect}
             />
           ))}
-        </ul>
-
-      </div>
-    );
-  };
+      </ul>
+    </div>
+  );
+};
 export default QuizHistory;
