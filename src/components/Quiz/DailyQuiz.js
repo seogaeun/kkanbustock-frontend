45b9052e069553cf456b1081bd0ec36519140b88
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-11 16:22:52 +0900

    [Add]오늘의 퀴즈 css 추가

--- src/components/Quiz/DailyQuiz.js
+++ src/components/Quiz/DailyQuiz.js
@@ -2,16 +2,18 @@ import React from 'react'
 import Header from '../Header/Header'
 import './DailyQuiz.css'
 
-function DailyQuiz() {
+function DailyQuiz(props) {
+    const { question, explanation} = props;
+
     return (
         <div className='dailyQuiz'>
-            <div className='title'>오늘의 퀴즈</div>
-            <div className='quiz'>
-                <div id='question'>매수란 주식을 파는 것을 의미한다.</div>
-                <div id='explanation'>주식을 파는 행위는 매도입니다.</div>
-                <div id='answer'>
-                    <button>O</button>
-                    <button>X</button>
+            <div className='quizTitle'>오늘의 퀴즈</div>
+            <div className='dailyQuizQnASet'>
+                <div id='question'>{question}</div>
+                <div id='explanation'>{explanation}</div>
+                <div className='oxbtn'>
+                    <button className='answer'>O</button>
+                    <button className='answer'>X</button>
                 </div>
             </div>
         </div>
--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './QuizPage.css'
 import Header from '../../components/Header/Header'
 import DailyQuiz from '../../components/Quiz/DailyQuiz'
+
 function Quiz() {
     return (
         <div>
             <Header name='퀴즈'/>
             <div className='dailyQuizSection'>
-                <DailyQuiz/>
+                <DailyQuiz
+                    question="매수란 주식을 파는 것을 의미한다."
+                    explanation="주식을 파는 행위는 매도입니다."
+                />
             </div>
         </div>
     )
