18b6c4266a19c669e5ba0f4160258afbfd70051b
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-11 20:49:14 +0900

    [Edit]오늘의 퀴즈 하단 사이즈 잘림 조정

--- src/components/Quiz/SolvedQuiz/SolvedQuiz.js
+++ src/components/Quiz/SolvedQuiz/SolvedQuiz.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './SolvedQuiz.css';
+import React from "react";
+import "./SolvedQuiz.css";
 
-
-const SolvedQuiz = ({ questionNumber, date, question, explanation, correctAnswer, isCorrect }) => {
+const SolvedQuiz = ({
+  questionNumber,
+  date,
+  question,
+  explanation,
+  correctAnswer,
+  isCorrect,
+}) => {
   return (
-    <div className={`solved-quiz ${isCorrect ? 'correct' : 'incorrect'}`}>
-      
-
-      <div className="solvedQuizItem">
-        <div>{questionNumber}</div>
-        <div>{date}</div>
-        <div>{question}</div>
-        <div>{explanation}</div>
-        <div>{correctAnswer}</div>
-        <div>{isCorrect ? '정답' : '오답'}</div>
+    <div className="solvedItem">
+      <div className={`solved-quiz ${isCorrect ? "correct" : "incorrect"}`}>
+        <div className="solvedQuizItem">
+          <div className="questionNumber">{questionNumber}</div>
+          <div className="date">{date}</div>
+          <div className="questionSet">
+            <div className="qustion">{question}</div>
+            <div className="explanation">{explanation}</div>
+          </div>
+          <div className="corretAnswer">{correctAnswer}</div>
+          <div className="isCorrect">{isCorrect ? "정답" : "오답"}</div>
+        </div>
       </div>
     </div>
   );
