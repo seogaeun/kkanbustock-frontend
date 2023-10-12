163cd2ae874450d133c66a9f3024e5cc0a8274aa
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-13 03:24:45 +0900

    [Edit]화살표 조정

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./MainPage.css";
 import Header from "../../components/Header/Header";
-import LL_together from "../../assets/images/LL_together.png";
 import MainPoint from "../../components/MainPoint/MainPoint";
 function Main() {
   return (
