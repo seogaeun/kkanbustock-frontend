4cfa5db0ba4c593ae80f03c72720bbdd30604edb
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-12 21:47:46 +0900

    [Edit]로고 간격 살짝 조정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import './Nav.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <div>
-            <div className='navbar'>
-                <div id='logo'>
-                    로고
-                </div>
-                <div className='menuClass'>
-                    <Link className='navbarMenu' to={'/'}>메인</Link>
-                    <Link className='navbarMenu' to={'/Rival'}>라이벌</Link>
-                    <Link className='navbarMenu' to={'/Dictionary'}>사전</Link>
-                    <Link className='navbarMenu' to={'/Quiz'}>퀴즈</Link>
-                    <Link className='navbarMenu' to={'/My'}>내정보</Link>
-                </div>
-            </div>
+  return (
+    <div className="nav">
+      <div className="navbar">
+        <div id="logo">로고</div>
+        <div className="menuClass">
+          <Link className="navbarMenu" to={"/"}>
+            메인
+          </Link>
+          <Link className="navbarMenu" to={"/Rival"}>
+            라이벌
+          </Link>
+          <Link className="navbarMenu" to={"/Dictionary"}>
+            사전
+          </Link>
+          <Link className="navbarMenu" to={"/Quiz"}>
+            퀴즈
+          </Link>
+          <Link className="navbarMenu" to={"/My"}>
+            내정보
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
