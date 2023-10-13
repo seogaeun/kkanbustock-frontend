5eac9bab3a2f87e7728fab510b92d58a1df5b5f8
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-13 17:58:41 +0900

    [Add]월간 수익률 컴포넌트 추가

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./MainPage.css";
 import Header from "../../components/Header/Header";
 import MainPoint from "../../components/MainPoint/MainPoint";
+import MonthlyRank from "../../components/MonthlyRank/MonthlyRank";
+
 function Main() {
   return (
     <div>
       <MainPoint />
-
+      <div>
+        <MonthlyRank />
+      </div>
       <Header name="안녕~" />
       <Header name="안녕~" />
       <Header name="안녕~" />
