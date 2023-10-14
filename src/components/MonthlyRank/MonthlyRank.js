3968e9c0228621e805d2ed9fe31d930df3b21469
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-14 17:42:15 +0900

    [Add[이번달 수익률 Top3 완성

--- src/components/MonthlyRank/MonthlyRank.js
+++ src/components/MonthlyRank/MonthlyRank.js
@@ -1,40 +1,33 @@
 import React from "react";
-import "./MonthlyRank.css"; // CSS 파일을 import
-import RankingCard from "../RankingCard/RankingCard.js";
+import "./MonthlyRank.css";
 import goldMedal from "../../assets/images/medal/gold.png";
 import silverMedal from "../../assets/images/medal/silver.png";
 import bronzeMedal from "../../assets/images/medal/dong.png";
 
 function MonthlyRank() {
-  // 데이터 리스트를 만듭니다.
   const data = [
     {
       id: 1,
       alt: "그룹 이미지 1",
       src: goldMedal,
       groupName: "그룹명 1",
-      groupRate: "수익률 1",
+      groupRate: "89%",
     },
     {
       id: 2,
       alt: "그룹 이미지 2",
       src: silverMedal,
       groupName: "그룹명 2",
-      groupRate: "수익률 2",
+      groupRate: "99%",
     },
     {
       id: 3,
       alt: "그룹 이미지 3",
       src: bronzeMedal,
       groupName: "그룹명 3",
-      groupRate: "수익률 3",
+      groupRate: "76%",
     },
   ];
-
-  // 첫 번째 데이터를 추출합니다.
-  const gold = data[0];
-  const silver = data[1];
-  const bronze = data[2];
 
   return (
     <div className="containerForMonthlyRanking">
@@ -42,28 +35,19 @@ function MonthlyRank() {
       <div className="headerForTitle">그룹별 / 2023.10.1-2023.10.31</div>
       <div className="medalList">
         <div className="silver">
-          <RankingCard
-            alt={silver.alt}
-            src={silver.src}
-            groupName={silver.groupName}
-            groupRate={silver.groupRate}
-          />
+          <img alt={data[1].alt} src={data[1].src} />
+          <div> {data[1].groupName} </div>
+          <div> 수익률: {data[1].groupRate} </div>
         </div>
         <div className="gold">
-          <RankingCard
-            alt={gold.alt}
-            src={gold.src}
-            groupName={gold.groupName}
-            groupRate={gold.groupRate}
-          />
+          <img alt={data[0].alt} src={data[0].src} />
+          <div> {data[0].groupName} </div>
+          <div> 수익률: {data[0].groupRate} </div>
         </div>
         <div className="bronze">
-          <RankingCard
-            alt={bronze.alt}
-            src={bronze.src}
-            groupName={bronze.groupName}
-            groupRate={bronze.groupRate}
-          />
+          <img alt={data[2].alt} src={data[2].src} />
+          <div> {data[2].groupName} </div>
+          <div> 수익률: {data[2].groupRate} </div>
         </div>
       </div>
     </div>
