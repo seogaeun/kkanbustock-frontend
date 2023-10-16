bde1b191ca772aa03af0624feac085d60e715da3
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-16 15:19:18 +0900

    [Edit]미저장된 코드 재저장

--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -3,6 +3,7 @@ import "./QuizPage.css";
 import Header from "../../components/Header/Header";
 import DailyQuiz from "../../components/Quiz/DailyQuiz/DailyQuiz.js";
 import QuizHistory from "../../components/Quiz/QuizHistory/QuizHistory";
+import axios from "axios";
 
 function Quiz() {
   // 임의의 퀴즈 데이터
@@ -24,6 +25,30 @@ function Quiz() {
       isCorrect: false,
     },
   ];
+
+  useEffect(() => {
+    // API에서 데이터를 가져오는 함수
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/v1/quizzes/daily/{memberId}"); // API 엔드포인트를 변경해야 할 수도 있습니다.
+        setDictionaryContents(response.data);
+        console.log("데이터 불러오기 성공");
+        console.log(response.data);
+      } catch (error) {
+        console.error("데이터 불러오기 실패", error);
+        // 실패 시에 대체 데이터 설정 가능
+        setDictionaryContents([
+          {
+            id: 1,
+            word: "데이터 불러오기 실패:(",
+            explanation: "데이터를 불러오지 못했습니다....:(",
+          },
+        ]);
+      }
+    };
+
+    fetchData();
+  }, []); // 빈 배열을 두어 컴포넌트가 처음 렌더링될 때만 데이터를 가져오도록 설정
 
   //임의의 오늘의 퀴즈 데이터
   const StockQuiz = [
