ca1240443a213cc18fef2a92ae0c2d4c7a5d9d51
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-17 19:55:14 +0900

    [Edit]카트 뒤집히는 효과 추가 및 ui수정

--- src/components/MainPage/dictionary-box.jsx
+++ src/components/MainPage/dictionary-box.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+// DictionaryBox.jsx
+
+import React, { useState } from 'react';
 import style from './styles/new-box.module.css';
 import commonStyles from './styles/main-page.module.css';
 import styles from "./styles/dictionary-box.module.css";
-import dicImg from '../../assets/images/SOL_search.png';
+import dicImg from '../../assets/images/SOL_Question.png';
 
 const DictionaryBox = ({ className, dict }) => {
     const { id, word, explanation } = dict || {};
 
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const toggleFlip = () => {
+        setIsFlipped(!isFlipped);
+    }
 
     return (
-        <div className={`${className ?? commonStyles.short_box} ${styles.container}`}>
-            <section className={styles.header}>
-                <div className={styles.title}>{word ?? "주식"}</div>
+        <div className={`${className ?? commonStyles.short_box} ${styles.container} ${isFlipped ? styles.flipped : ''}`} onMouseEnter={toggleFlip} onMouseLeave={toggleFlip}>
+            <section className={`${styles.side} ${styles.front}`}>
+                <div className={styles.title}>
+                    {word ?? "주식이란?"}
+                    <span className={styles.smallText}> 의 뜻을 <br /> 아시나요?</span>
+                </div>
+            </section>
+            <section className={`${styles.side} ${styles.back}`}>
                 <div className={styles.content}>{explanation ?? "회사의 자본을 이루는 단위"}</div>
             </section>
             <section className={styles.image_box}>
