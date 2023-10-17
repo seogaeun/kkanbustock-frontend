35264f98e80e241f96368d37ac5dd5f3a7b0c71c
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-10-17 21:24:18 +0900

    [Add]사전 API 연결 및 UI 완성! 페이지 이동도 추가

--- src/components/MainPage/dictionary-box.jsx
+++ src/components/MainPage/dictionary-box.jsx
@@ -1,34 +1,45 @@
-// DictionaryBox.jsx
-
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './styles/new-box.module.css';
 import commonStyles from './styles/main-page.module.css';
-import styles from "./styles/dictionary-box.module.css";
+import styles from './styles/dictionary-box.module.css';
 import dicImg from '../../assets/images/SOL_Question.png';
 
 const DictionaryBox = ({ className, dict }) => {
     const { id, word, explanation } = dict || {};
 
     const [isFlipped, setIsFlipped] = useState(false);
+    const navigate = useNavigate();
 
     const toggleFlip = () => {
         setIsFlipped(!isFlipped);
     }
 
+    const handleCardClick = () => {
+        // Redirect to the dictionary page when the card is clicked
+        navigate('/Dictionary');
+    }
+
     return (
-        <div className={`${className ?? commonStyles.short_box} ${styles.container} ${isFlipped ? styles.flipped : ''}`} onMouseEnter={toggleFlip} onMouseLeave={toggleFlip}>
+        <div
+            className={`${className ?? commonStyles.short_box} ${styles.container} ${isFlipped ? styles.flipped : ''}`}
+            onMouseEnter={toggleFlip}
+            onMouseLeave={toggleFlip}
+            onClick={handleCardClick} // Add click event to navigate on card click
+            style={{ cursor: 'pointer' }} // Change cursor to pointer
+        >
             <section className={`${styles.side} ${styles.front}`}>
                 <div className={styles.title}>
-                    {word ?? "주식이란?"}
-                    <span className={styles.smallText}> 의 뜻을 <br /> 아시나요?</span>
+                    '{word ?? "주식"} '
+                    <span className={styles.smallText}> 뜻?</span>
                 </div>
             </section>
             <section className={`${styles.side} ${styles.back}`}>
                 <div className={styles.content}>{explanation ?? "회사의 자본을 이루는 단위"}</div>
             </section>
-            <section className={styles.image_box}>
+            <section className={`${styles.image_box} ${styles.side} ${styles.front}`} >
                 <div className={`${styles.short_box_image_box}`}>
-                    <img className={`${styles.short_box_image}`} src={dicImg} alt={"사전"} />
+                    <img className={`${styles.short_box_image}`} src={dicImg} alt="사전" />
                 </div>
             </section>
         </div>
--- src/components/MainPage/dictionaty-box2.jsx
+++ src/components/MainPage/dictionaty-box2.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './styles/new-box.module.css';
 import commonStyles from './styles/main-page.module.css';
-import styles from "./styles/dictionary-box2.module.css";
+import styles from './styles/dictionary-box2.module.css';
 import dicImg from '../../assets/images/SOL_search.png';
-const DictionaryBox2 = ({className, dict}) => {
-const { id, word, explanation } = dict || {};
+
+const DictionaryBox2 = ({ className, dict }) => {
+    const { id, word, explanation } = dict || {};
+    const navigate = useNavigate();
+
+    const handleCardClick = () => {
+        // Redirect to the dictionary page when the card is clicked
+        navigate('/Dictionary');
+    }
+
     return (
-        <div className={`${className ?? commonStyles.flex_box} ${styles.container2}`}>
-          <section className={styles.header2}>
-            <div className={styles.title2}>{word ?? "주식"}</div>
-            <div className={styles.content2}>{explanation ?? "회사의 자본을 이루는 단위"}</div>
-          </section>
-          <section className={styles.image_box2}>
-            <div className={`${styles.short_box_image_box2}`}>
-              <img className={`${styles.short_box_image2}`} src={dicImg} alt={"사전"}/>
-            </div>
-          </section>
+        <div
+            className={`${className ?? commonStyles.flex_box} ${styles.container2}`}
+            onClick={handleCardClick}
+            style={{ cursor: 'pointer' }}
+        >
+            <section className={styles.image_box2}>
+                <div className={`${styles.short_box_image_box2}`}>
+                    <img className={`${styles.short_box_image2}`} src={dicImg} alt="사전" />
+                </div>
+            </section>
+            <section className={styles.header2}>
+                <div className={styles.title2}>' {word ?? "주식"} '의 의미는?</div>
+                <div className={styles.content2}>{explanation ?? "회사의 자본을 이루는 단위"}</div>
+            </section>
         </div>
     );
 };
