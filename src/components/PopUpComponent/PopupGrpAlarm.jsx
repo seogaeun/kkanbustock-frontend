af428e591f65e5f2401d0d3b37f55ee8aed69b14
seogaeun <se0323333@gmail.com>
2023-10-19 03:47:23 +0900

    [ADD] 그룹 승인 버튼 추가

--- src/components/PopUpComponent/PopupGrpAlarm.jsx
+++ src/components/PopUpComponent/PopupGrpAlarm.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import './PopupPortGrpAlarm.css';
 import PopupComponent from './PopupComponent';
-
+import GrpAcceptItem from '../GrpAccept/GrpAcceptItem';
 
 function PopupPortGrpAlarm({userInfo,stockDataList,isOpen,closeModal}) {
 
   return (
             <PopupComponent isOpen={isOpen} closeModal={closeModal}>
                 <div className='PopupPortGrpAlarmWrap'>
+                    <div className="PopupPortRecommandStock-Title">My 그룹 알림</div>
                     <div className="PopupPortGrpAlarm-upContent">
+                      <GrpAcceptItem hostName="서가돌" hostId="gadol"></GrpAcceptItem>
                     </div>
 
                 </div>
