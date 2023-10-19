a788639e3bc3acd8a6eb1e79f3fc46277c0ac15f
sangeun <grace0857@naver.com>
2023-10-19 22:40:31 +0900

    test

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -16,7 +16,7 @@ function SignUpPage() {
 
 
     const handleSignUp = async () => {
-        axios.post('http://service.team-4.svc.cluster.local:8080/api/v1/register', {
+        axios.post('http://app-team-4.shinhansec-pda.net/api/v1/register', {
             id: id,
             name: name,
             email: email,
