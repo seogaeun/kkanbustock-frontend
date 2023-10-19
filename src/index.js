712dc89c6aea910be8d183d367d927534a51263a
ghdeo <goldencorn7@naver.com>
2023-10-20 01:50:21 +0900

    [FEAT] getAuth 추가

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<App/>);
 
+const getAuth = (token) => axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": `application/json;charset=UTF-8`,
+        "Accept": "application/json",
+        "Authorization": "Bearer "+token,
+
+        // 추가
+        "Access-Control-Allow-Origin": `http://localhost:3000`,
+        'Access-Control-Allow-Credentials':"true",
+    }
+})
