7e9f91704da639e6a8c4bff12070b6fcb21bd5fa
ghdeo <goldencorn7@naver.com>
2023-10-20 03:19:52 +0900

    [FEAT] index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'axios'
+import axios from 'axios'
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.withCredentials = true;
 
+const getAuth = () => axios.create({
+    baseURL: 'https://app-team-4.shinhansec-pda.net',
+    headers: {
+        "Content-Type": `application/json;charset=UTF-8`,
+        "Accept": "application/json",
+        "Authorization": "Bearer ",
+
+        // 추가
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Credentials':"true",
+    }
+})
 
 root.render(<App/>);
-
-// const getAuth = (token) => axios.create({
-//     baseURL: "app-team-4.shinhansec-pda.net",
-//     headers: {
-//         "Content-Type": `application/json;charset=UTF-8`,
-//         "Accept": "application/json",
-//         "Authorization": "Bearer "+token,
-//
-//         // 추가
-//         "Access-Control-Allow-Origin": "*",
-//         'Access-Control-Allow-Credentials':"true",
-//     }
-// })
