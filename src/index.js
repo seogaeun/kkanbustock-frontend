a11f2d7651c984836cf866edc955aa88843704d3
sangeun <grace0857@naver.com>
2023-10-19 19:08:18 +0900

    api 연결

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import PopupComponent from './components/PopUpComponent/PopupComponent';
-import InvestTestBox from './components/MainPage/invest-test-box';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
